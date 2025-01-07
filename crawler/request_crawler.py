dd5aa4f5ec857e689c200d835335658839ec3a33
thinh <thinhquangshin@gmail.com>
2025-01-08 01:53:53 +0700

    feat: crawl by call api
    add request_crawler

---
+++ crawler/request_crawler.py
@@ -0,0 +1,76 @@
+import requests
+import traceback 
+from sink.sink import Sink
+import json
+import time
+from util import data_util
+import copy
+
+class RequestCrawler():
+    def __init__(self, pipeline):
+        self.pipeline = pipeline
+
+    def get(self, input_val ,**kwargs):
+        try:
+            params = data_util.format_params(kwargs.get("params"), kwargs)
+            response = requests.get(
+                kwargs.get("url"), 
+                params=params, 
+                headers=kwargs.get("headers"), 
+                data=kwargs.get("data"), 
+                cookies=kwargs.get("cookies")
+            )
+            if kwargs.get("format"):
+                result = response.__getattribute__(kwargs.get("format"))()
+            else:
+                result = response.raw
+            if kwargs.get("cond"):
+                expr = kwargs["cond"]["validate"]
+                if eval(expr):
+                    exit()
+            return result
+        except Exception as e:
+            traceback.print_exc()
+    
+    def save(self, input_val, **kwargs):
+        Sink(kwargs.get("type"), input_val, **kwargs).execute()
+    
+    def foreach(self, input_val, **kwargs):
+        iterator = []
+        if isinstance(input_val, list):
+            iterator = input_val
+        if kwargs.get("limit"):
+            limit = kwargs.get("limit")
+            start = 0
+            step = 1
+            if kwargs.get("start"):
+                start  = kwargs.get("start")
+            if kwargs.get("step"):
+                step = kwargs.get("step")
+            iterator = range(start, limit, step)
+            
+        for item in iterator:
+            actions = copy.deepcopy(kwargs["actions"])
+            tmp_val = item
+            for action in actions:
+                try:
+                    params = action["params"]
+                    params["item"] = item
+                    if kwargs.get("cond"):
+                        if kwargs["cond"]["action"] == action.get("name"):
+                            params["cond"] = kwargs["cond"]
+                    tmp_val = getattr(self,action.get("name"))(tmp_val, **params)
+                except Exception as e:
+                    traceback.print_exc()
+            if kwargs.get("delay"):
+                time.sleep(kwargs.get("delay"))
+        return tmp_val
+    
+    def run(self):
+        tmp_val = None
+        for action in self.pipeline:
+            method = getattr(self, action.get("name"))
+            tmp_val = method(tmp_val, **action.get("params"))
+        return tmp_val
+    
+
